5a82274083bd1fcf7e293830ce617fb9162ef92b
Santo <santobiswas0011@gmail.com>
2023-10-17 01:34:13 +0530

    update 17/10/2023

--- server/server.js
+++ server/server.js
@@ -30,3 +30,18 @@ app.use('/api', require('./routes/Dashborad/cartRouter'));
 const port = process.env.PORT;
 dbConnect();
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
+
+
+
+/* PORT=8000
+DB_URL='mongodb://localhost:27017/posApplication'
+SECRET = santobiswas
+COOKIE_EXP = 7
+USER_EMAIL  = 'santobiswas0011@gmail.com'
+USER_PASSWORD  = 'santosantosanto'
+TOKEN_EXP  = '7d'
+
+cloud_name = dfv1bunei
+api_key = 347556769576843
+api_secret = _mOWawr4AQ7G_VfByU9q51ApvAY
+ */
